fade4d2f68f7e3a5e8e3c6e38e12a62e8179d823
erezzoug <rezougelmehdi0@gmail.com>
2026-02-28 13:24:06 +0000

    fixing printing shit in main before error

--- main.go
+++ main.go
@@ -23,18 +23,20 @@ func main() {
 		return
 	}
 
-	for _, line := range colony.RawLines {
-		fmt.Println(line)
-	}
-	fmt.Println()
-
 	paths, err := utils.FindPaths(colony)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "ERROR:", err)
 		return
 	}
 
-	for _, move := range utils.Simulate(paths, colony.NumAnts) {
+	moves := utils.Simulate(paths, colony.NumAnts)
+
+	// All good — print everything
+	for _, line := range colony.RawLines {
+		fmt.Println(line)
+	}
+	fmt.Println()
+	for _, move := range moves {
 		fmt.Println(move)
 	}
 	fmt.Fprintf(os.Stderr, "\nTime: %v\n", time.Since(start))
